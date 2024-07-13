06e0e81caab9a861d9254a695672d8ddfa59eb5d
tegacodess <tegazion7@gmail.com>
2024-07-13 15:33:02 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+ // JavaScript function to get the current year and insert it into the HTML
+ function displayCurrentYear() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+}
+function myFunction() {
+    var myLinks = document.getElementById("myLinks");
+    var homeLink = document.querySelector('.topnav .home');
+    if (myLinks.style.display === "block") {
+        myLinks.style.display = "none";
+        homeLink.style.display = "block";
+    } else {
+        myLinks.style.display = "block";
+        homeLink.style.display = "none";
+   }
+ }
+
+ // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+        
+function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     document.getElementById("myBtn").style.display = "block";
+   } else {
+     document.getElementById("myBtn").style.display = "none";
+   }
+}
+ 
+ // Functionality for back to top button
+function topFunction() {
+   document.body.scrollTop = 0; // For Safari
+   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
